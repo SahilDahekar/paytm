5be6e15b8473f416ab07fccf8339ff8a6bda2b44
SahilDahekar <dahekar30sahil@gmail.com>
2024-01-21 21:45:24 +0530

    Added backend for paytm assignment

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,17 @@
 const express = require("express");
+const app = express();
+const rootRouter = require("./routes/index");
+const cors = require("cors");
+const { connectdB } = require("./db");
 
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/v1', rootRouter);
+
+connectdB();
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+})
 
